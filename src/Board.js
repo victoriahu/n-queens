a41260491156dd8b76ab0c0f150340296bad98a4
victoriahu <vh12490@gmail.com>
2018-11-14 20:23:50 -0800

    add major diagonal

--- src/Board.js
+++ src/Board.js
@@ -176,11 +176,50 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+      // d = majorDiagonalColumnIndexAtFirstRow
+      // intialize count to 0
+      // for i from 0 to row.length - 1 - d
+      //   if row[i][i + d] equals 1
+      //     increment count
+      //     if count equals 2
+      //        return true
+      // return false
+      var rows = this.rows();
+      var d = majorDiagonalColumnIndexAtFirstRow;
+      var count = 0; 
+      var n = rows.length;
+      var ilast = n - 1 - d;
+      if (ilast > n - 1) {
+        ilast = n - 1;
+      }
+      for (var i = 0; i <= ilast; i++) {
+        debugger;
+        if (rows[i][i + d] === 1) {
+          count++;
+          if (count === 2) {
+            return true;
+          }
+        }
+      }
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      // get rows
+      // n = rows.length
+      // for d from -(n-1) up to but not including n 
+      //   if hasMajorDConflict at d
+      //     return true
+      // return false
+
+      var rows = this.rows();
+      var n = rows.length;
+      for (var d = -(n - 1); d < n; d++) {
+        if (this.hasMajorDiagonalConflictAt(d)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
